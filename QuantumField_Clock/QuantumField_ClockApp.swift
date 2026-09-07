2518b0afaf45fccbbf0b0217e6e881e5408ca271
tomglander <9cjb4njp6n@privaterelay.appleid.com>
2026-09-07 10:43:45 -0700

    Initial Commit

---
+++ QuantumField_Clock/QuantumField_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuantumField_ClockApp.swift
+//  QuantumField_Clock
+//
+//  Created by Tom Glander on 9/7/26.
+//
+
+import SwiftUI
+
+@main
+struct QuantumField_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
